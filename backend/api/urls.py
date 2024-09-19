b16a2ad9e4a8b685701e7d4bed71e68f756545df
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-09-19 19:27:37 +0300

    update urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path('recipes/', include('api.recipes.urls')),
-    path('users/', include('api.users.urls')),
+    path('', include('api.recipes.urls')),
+    path('', include('api.users.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -21,9 +21,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.authtoken')),
-    path('api/v1/', include('api.urls')),
+    path('api/', include('api.urls')),
 ]
 
 if settings.DEBUG:
