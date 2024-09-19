b37c968ac12fd3b8602a1d40d2d4e4bd1f135257
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-09-19 19:56:23 +0300

    update urls

--- backend/api/users/urls.py
+++ backend/api/users/urls.py
@@ -1,13 +1,13 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+#from django.urls import include, path
+#from rest_framework.routers import DefaultRouter
 
-from .views import FollowViewSet, UserViewSet
+#from .views import FollowViewSet, UserViewSet
 
-v1_router = DefaultRouter()
-v1_router.register(r'follow', FollowViewSet, basename='follow')
-v1_router.register(r'users', UserViewSet, basename='users')
+#v1_router = DefaultRouter()
+#v1_router.register(r'follow', FollowViewSet, basename='follow')
+#v1_router.register(r'users', UserViewSet, basename='users')
 
 
-urlpatterns = [
-    path('', include(v1_router.urls)),
-]
+#urlpatterns = [
+   # path('', include(v1_router.urls)),
+#]
