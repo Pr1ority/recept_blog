cfbf3ccc597d92ae96fabc83f044ce90ca732fc4
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-09-19 20:24:47 +0300

    update urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,19 +1,6 @@
 from django.urls import include, path
-from recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
-from rest_framework.routers import DefaultRouter
-from users.views import FollowViewSet, UserViewSet
-
-router = DefaultRouter()
-router.register(r'recipes', RecipeViewSet, basename='recipes')
-router.register(r'tags', TagViewSet, basename='tags')
-router.register(r'users', UserViewSet, basename='users')
-router.register(
-    r'ingredients', IngredientViewSet, basename='ingredients')
-router.register(
-    r'follow', FollowViewSet, basename='follow')
-
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path(r'auth/', include('djoser.urls.authtoken')),
+    path('recipes/', include('api.recipes.urls')),
+    path('users/', include('api.users.urls')),
 ]
