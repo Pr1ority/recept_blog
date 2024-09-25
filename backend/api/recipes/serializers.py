ec4d71729b029f99e703a8f5bee74f0c61e87790
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-09-25 17:32:15 +0300

    update serializers

--- backend/api/recipes/serializers.py
+++ backend/api/recipes/serializers.py
@@ -56,40 +56,6 @@ class RecipeSerializer(serializers.ModelSerializer):
         return serializer.data
 
 
-    def create_ingredients(self, recipe, ingredients_data):
-        for ingredient in ingredients_data:
-            RecipeIngredient.objects.create(
-                recipe=recipe,
-                ingredient_id=ingredient['id'],
-                amount=ingredient['amount']
-            )
-
-    def create(self, validated_data):
-        tags_data = validated_data.pop('tags')
-        ingredients_data = validated_data.pop('ingredients')
-        recipe = Recipe.objects.create(**validated_data)
-        recipe.tags.set(tags_data)
-        self.create_ingredients(recipe, ingredients_data)
-        return recipe
-
-    def update(self, instance, validated_data):
-        tags_data = validated_data.pop('tags')
-        ingredients_data = validated_data.pop('ingredients')
-
-        instance.name = validated_data.get('title', instance.name)
-        instance.text = validated_data.get('description', instance.text)
-        instance.image = validated_data.get('image', instance.image)
-        instance.cooking_time = validated_data.get('cooking_time',
-                                                   instance.cooking_time)
-        instance.save()
-
-        instance.tags.set(tags_data)
-        instance.ingredients.clear()
-        self.create_ingredients(instance, ingredients_data)
-
-        return instance
-
-
 class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Favorite
