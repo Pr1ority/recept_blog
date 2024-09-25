66fb5692d205d88bf8af86543c506148997973a6
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-09-25 18:00:05 +0300

    update serializers

--- backend/api/recipes/serializers.py
+++ backend/api/recipes/serializers.py
@@ -10,6 +10,15 @@ from rest_framework import serializers
 User = get_user_model()
 
 
+class RecipeIngredientCreateSerializer(serializers.ModelSerializer):
+    id = serializers.PrimaryKeyRelatedField(queryset=Ingredient.objects.all())
+    amount = serializers.IntegerField()
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ['id', 'amount']
+
+
 class Base64ImageField(serializers.ImageField):
     def to_internal_value(self, data):
         if isinstance(data, str) and data.startswith('data:image'):
@@ -41,7 +50,7 @@ class IngredientSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(read_only=True)
     tags = TagSerializer(many=True)
-    ingredients = IngredientSerializer(many=True)
+    ingredients = RecipeIngredientCreateSerializer(many=True)
     image = Base64ImageField()
 
     class Meta:
@@ -51,12 +60,38 @@ class RecipeSerializer(serializers.ModelSerializer):
             'tags', 'cooking_time', 'pub_date'
         )
 
-    def get_ingredients(self, obj):
-        ingredients = RecipeIngredient.objects.filter(recipe=obj)
-        serializer = RecipeIngredientSerializer(ingredients, many=True)
+    def create_ingredients(self, recipe, ingredients_data):
+        for ingredient in ingredients_data:
+            RecipeIngredient.objects.create(
+                recipe=recipe,
+                ingredient_id=ingredient['id'],
+                amount=ingredient['amount']
+            )
 
-        return serializer.data
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags')
+        ingredients_data = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(**validated_data)
+        recipe.tags.set(tags_data)
+        self.create_ingredients(recipe, ingredients_data)
+        return recipe
 
+    def update(self, instance, validated_data):
+        tags_data = validated_data.pop('tags')
+        ingredients_data = validated_data.pop('ingredients')
+
+        instance.name = validated_data.get('title', instance.name)
+        instance.text = validated_data.get('description', instance.text)
+        instance.image = validated_data.get('image', instance.image)
+        instance.cooking_time = validated_data.get('cooking_time',
+                                                   instance.cooking_time)
+        instance.save()
+
+        instance.tags.set(tags_data)
+        instance.ingredients.clear()
+        self.create_ingredients(instance, ingredients_data)
+
+        return instance
 
 
 class FavoriteSerializer(serializers.ModelSerializer):
