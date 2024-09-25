98688658bd9109dad707be26863374616c72627f
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-09-25 14:25:53 +0300

    update settings

--- backend/tags/models.py
+++ backend/tags/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=200, unique=True, verbose_name='Имя')
-    color = models.CharField(max_length=7, verbose_name='Цвет')
+    name = models.CharField(max_length=200, unique=True, verbose_name='Название')
     slug = models.SlugField(unique=True, verbose_name='Слаг')
 
     def __str__(self):
