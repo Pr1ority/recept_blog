1f73a9475051d36115ebd020136ae14b2cfa65fd
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-10-03 08:45:22 +0300

    update models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -25,8 +25,8 @@ class Tag(models.Model):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=128, verbose_name='Имя')
-    measurement_unit = models.CharField(max_length=50,
-                                        verbose_name='Единица измерения', max_length=64)
+    measurement_unit = models.CharField(verbose_name='Единица измерения',
+                                        max_length=64)
 
     def __str__(self):
         return self.name
@@ -42,7 +42,8 @@ class Ingredient(models.Model):
 
 
 class Recipe(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               verbose_name='Автор')
     name = models.CharField(max_length=256, verbose_name='Название')
     image = models.ImageField(upload_to='recipes/images/',
                               verbose_name='Изображение')
@@ -53,7 +54,8 @@ class Recipe(models.Model):
     tags = models.ManyToManyField(Tag,
                                   verbose_name='Теги')
     cooking_time = models.PositiveIntegerField(
-        help_text='Время в минутах', verbose_name='Время приготовления', validators=validate_cooking_time)
+        help_text='Время в минутах', verbose_name='Время приготовления',
+        validators=validate_cooking_time)
     pub_date = models.DateTimeField(auto_now_add=True,
                                     verbose_name='Дата публикации')
 
@@ -74,7 +76,8 @@ class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE,
                                    verbose_name='Продукт',
                                    related_name='in_recipe')
-    amount = models.PositiveIntegerField('Количество', validators=validate_ingredients)
+    amount = models.PositiveIntegerField('Количество',
+                                         validators=validate_ingredients)
 
     def __str__(self):
         return f'{self.amount} {self.ingredient.name} для {self.recipe.name}'
@@ -85,7 +88,7 @@ class RecipeIngredient(models.Model):
 
 
 class Favorite(UserRecipeBase):
-    
+
     class Meta:
         verbose_name = 'избранное'
 
@@ -106,7 +109,8 @@ class User(AbstractUser):
         unique=True,
         validators=[RegexValidator(
             regex=r'^[\w.@+-]+\z',
-            message=('Ник содержит недопустимые символы. Допустимы только буквы, цифры, и символы @/./+/-/_.'),
+            message=('Ник содержит недопустимые символы.'
+                     ' Допустимы только буквы, цифры, и символы @/./+/-/_.'),
         )],
         verbose_name='Имя пользователя'
     )
@@ -128,7 +132,8 @@ class Follow(models.Model):
                              related_name='followers',
                              verbose_name='Пользователь')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name='followings', verbose_name='Авторы')
+                               related_name='followings',
+                               verbose_name='Авторы')
 
     class Meta:
         constraints = [
