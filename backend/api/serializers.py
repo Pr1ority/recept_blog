e35bd88eaf1e0b656e86aff9344bebfb5a273759
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-10-09 02:23:09 +0300

    update serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -44,11 +44,6 @@ class RecipeIngredientCreateSerializer(serializers.ModelSerializer):
                 'measurement_unit': ingredient.measurement_unit,
                 'amount': instance.amount,
             }
-        else:
-            return {
-                'error': 'Unexpected instance type', 
-                'instance_type': type(instance).__name__
-            }
 
 
 class TagSerializer(serializers.ModelSerializer):
