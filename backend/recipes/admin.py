e459d772e5a64c48492696b552363f575fc0abeb
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-10-08 14:44:16 +0300

    update admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -57,6 +57,7 @@ class RecipeAdmin(admin.ModelAdmin):
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
     search_fields = ('name',)
+    list_filter = ('measurement_unit',)
 
 
 @admin.register(RecipeIngredient)
